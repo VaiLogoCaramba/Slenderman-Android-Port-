5a2c31cc56f39434e8bac6e3bf36b8a4471a11f4
VaiLogoCaramba <150534279+VaiLogoCaramba@users.noreply.github.com>
2024-03-11 23:05:06 -0300

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -133,6 +133,10 @@ class PauseSubState extends MusicBeatSubstate
 
 		regenMenu();
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
+
+		#if android
+		addVirtualPad(UP_DOWN, A_B);
+		#end
 	}
 
 	var holdTime:Float = 0;
